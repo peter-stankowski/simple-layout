8ad4600b746cd362e3103f2bead9780919fc2195
JoshCrz <35774174+JoshCrz@users.noreply.github.com>
2019-05-19 22:43:15 +0100

    form builder component

--- MyApp.AngularClient/ClientApp/src/app/forms/components/form-builder/form-builder.component.ts
+++ MyApp.AngularClient/ClientApp/src/app/forms/components/form-builder/form-builder.component.ts
@@ -1,16 +1,37 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-form-builder',
+  selector: 'form-builder',
   templateUrl: './form-builder.component.html',
   styleUrls: ['./form-builder.component.css']
 })
 export class FormBuilderComponent implements OnInit {
 
+  newForm: FormGroup;
+  newControl: FormGroup;
+
   constructor() { }
 
   ngOnInit() {
+
+    //construct an empty form group
+
+    this.newForm = new FormGroup ({
+
+    })
+  }
+
+  createNewControl() {
+    //create/set new form control of type formGroup
+    this.newControl = new FormGroup({
+      label: new FormControl('', Validators.required),
+      type: new FormControl(Validators.required)
+    })    
+  }
+
+  saveNewControl(form) {
+    console.log(form)
   }
 
 }
--- MyApp.AngularClient/ClientApp/src/app/forms/forms.module.ts
+++ MyApp.AngularClient/ClientApp/src/app/forms/forms.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModules } from '../shared/modules/material.module';
 import { FormsComponent } from './forms.component';
+import { FormBuilderComponent } from './components/form-builder/form-builder.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    MaterialModules,
+    MaterialModules
   ],
   declarations: [
-    FormsComponent
+    FormsComponent,
+    FormBuilderComponent
   ],
   exports: [
     FormsComponent
